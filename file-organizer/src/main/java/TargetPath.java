c69c5cf53e28be78deceec07ad8775d44e22c2f5
winrid <winrid@gmail.com>
2024-09-11 07:58:26 -0700

    fix

--- file-organizer/src/main/java/TargetPath.java
+++ file-organizer/src/main/java/TargetPath.java
@@ -1,6 +1,8 @@
+import java.io.Serializable;
 import java.nio.file.Path;
 
-public class TargetPath {
+public class TargetPath implements Serializable {
+    private static final long serialVersionUID = 1L;
     final Path sourcePath;
     final Path targetPath;
 
